ecc1241a3b962d71d50b7f871424d1228e0f2abe
Caio <caiovr13@gmail.com>
2021-04-12 19:03:29 -0300

    Configuração inicial + Alert de permissão de localização

--- Pokemon GO/ViewController.swift
+++ Pokemon GO/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == .denied {
+            presentNeedLocationAlert()
+        }
+    }
+    
+    func presentNeedLocationAlert(){
+        let alert = UIAlertController(title: "Localization Permission", message: "We need that you allow localization service to app works fine", preferredStyle: .alert)
+        
+        let openSettingsAction = UIAlertAction(title: "Open settings", style: .default, handler: {(UIAlertAction) -> Void in
+            
+            if let path = NSURL(string: UIApplication.openSettingsURLString){
+                UIApplication.shared.open(path as URL)
+            }
+            
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+        alert.addAction(openSettingsAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
