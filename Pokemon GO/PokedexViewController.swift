8c3eaf9f6ebf753a7ccdaac5f2a701a98c67de20
Caio <caiovr13@gmail.com>
2021-04-15 00:34:53 -0300

    Apresentação dos pokes na table view

--- Pokemon GO/PokedexViewController.swift
+++ Pokemon GO/PokedexViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class PokedexViewController: UIViewController {
-
+class PokedexViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     var caughtPokes: [Pokemon] = []
     var uncaughtPokes: [Pokemon] = []
 
@@ -21,5 +21,45 @@ class PokedexViewController: UIViewController {
         let coreData = CoreDataPokemon()
         caughtPokes = coreData.retrievePokedexPokes(caught: true)
         uncaughtPokes = coreData.retrievePokedexPokes(caught: false)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Caught"
+        } else {
+            return "Uncaught"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return caughtPokes.count
+        } else {
+            return uncaughtPokes.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "reuseCell")
+        
+        var pokemon: Pokemon
+        var catchesText: String = ""
+        
+        if indexPath.section == 0 {
+            pokemon = caughtPokes[indexPath.row]
+            catchesText = "Catches: " + String(pokemon.catches)
+        } else {
+            pokemon = uncaughtPokes[indexPath.row]
+        }
+        
+        cell.textLabel?.text = pokemon.name
+        cell.detailTextLabel?.text = catchesText
+        cell.imageView?.image = UIImage(named: pokemon.imageName!)
+        
+        return cell
     }
 }
