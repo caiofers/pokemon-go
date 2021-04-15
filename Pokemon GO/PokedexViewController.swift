b287058f5da8a3aa1f2949cb10f698c4bd3c6d0a
Caio <caiovr13@gmail.com>
2021-04-14 23:37:55 -0300

    PokedexViewController

---
+++ Pokemon GO/PokedexViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PokedexViewController.swift
+//  Pokemon GO
+//
+//  Created by Caio Fernandes on 14/04/21.
+//
+
+import UIKit
+
+class PokedexViewController: UIViewController {
+
+    @IBAction func backToMap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
